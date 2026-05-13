e3b64dfd116dbec88eb09e36f5f16e84d40b73eb
RamyarPC <ramyarburhan26@gmail.com>
2026-05-13 01:06:34 +0100

    no animations

--- steps/03_workspaces.sh
+++ steps/03_workspaces.sh
@@ -2,7 +2,7 @@
 
 run_workspaces() {
     step_start "Configuring workspace keybindings"
-
+	gsettings set org.gnome.desktop.interface enable-animations false
     for index in {1..9}; do
         gsettings set org.gnome.shell.keybindings "switch-to-application-$index" "[]"
         gsettings set org.gnome.desktop.wm.keybindings "switch-to-workspace-$index" "['<Super>$index', '<Alt>$index']"
