798e65f0c388a720b8d897d80a979c9e2af70599
RamyarPC <ramyarburhan26@gmail.com>
2026-05-13 02:48:00 +0100

    more essential apps

--- steps/02_keyboard.sh
+++ steps/02_keyboard.sh
@@ -3,7 +3,6 @@
 run_keyboard() {
     step_start "Configuring keyboard settings"
 
-    gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
     gsettings set org.gnome.desktop.peripherals.keyboard delay 200
     gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 40
 
